96b716024db8fbd4e2a485b03c807714657c3dd7
Wilrey <wilrey@gmail.com>
2023-07-17 12:39:15 +0200

    Update call.js

--- call.js
+++ call.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector("#login");
   const phoneInput = document.querySelector("#phone");
+  const info = document.querySelector(".alert");
+
   form.addEventListener("submit", process);
 
   function process(event) {
     event.preventDefault();
     const phoneNumber = phoneInput.value;
-    alert("Phone number in E.164 format: <strong>" + phoneNumber + "</strong>");
+    info.classList.remove("hidden");
+    info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
   }
 });
-
